ab8310824212faeb745e1d67989c5424b6e87490
felipesayegg <felipesayeg2018@gmail.com>
2025-09-13 17:13:42 -0300

    Implementa TextoDinamico

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import BotaoEvento from "./components/BotaoEvento";
+import TextoDinamico from "./components/TextoDinamico";
 export default function App() {
   return (
     <main style={{ fontFamily: "sans-serif", padding: 16 }}>
-      <BotaoEvento />
+      <TextoDinamico />
     </main>
   );
 }
